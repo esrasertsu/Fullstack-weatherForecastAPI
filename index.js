9fcee09da3167239e2f96742865db0251800f57a
esrasertsu <esrasertsu@gmail.com>
2019-04-08 01:08:20 +1000

    mongoose

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/Users');
+
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true });
+
+
+// mongoose.connect(keys.mongoURI, function(err,db){
+//      if (err) throw err;
+//         else console.log(db);
+      
+// });
 
 const app = express();
 require('./routes/authRoutes')(app);
