1a93471b1498f0d3966786a96c4cebb229b03eff
Sertsu <esertsu@SDYL161c55cd3.local>
2019-06-27 16:43:12 +1000

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,10 @@ module.exports = (app) => {
   //google Strategy name is 'google' general rule
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout',(req,res)=> {
+    req.logout();
+    res.send(req.user);
+  });
   app.get('/api/current_user', (req,res) => {
     res.send(req.user);
   });
